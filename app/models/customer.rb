6ccefb2e9f5519ef9e4a54949e58c27abc325ecd
Javi <javsteratii@gmail.com>
2019-10-15 10:36:58 -0400

    Javi

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
+  def self.all
+@@all
+  end 
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self 
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    @restaurant = restaurant
+    @content = content
+    @rating = rating 
+    #given a restaurant object, some review content (as a string), 
+    #and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+  end 
+
+  def num_reviews
+    #Returns the total number of reviews that a customer has authored
+  end 
+    
+  def find_by_name(name)
+    #given a string of a full name, returns the first customer whose full name matches
+  end 
+
+  def find_all_by_first_name(name)
+    #given a string of a first name, returns an array containing all customers with that first name
+  end 
+
+  def all_names
+    #should return an array of all of the customer full names
+  end 
+    
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,21 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+customer1 = Customer.new("Billy", "Bob")
+customer2 = Customer.new("Billy", "Jean")
+customer3 = Customer.new("Robby", "Bobby")
+customer4 = Customer.new("Gucci", "Mane")
+
+rest1 = Restaurant.new("Jacks!")
+rest2 = Restaurant.new("Bills!")
+rest3 = Restaurant.new("Waffle House!")
+rest4 = Restaurant.new("Chipotle!")
+
+
+customer1.add_review(rest1, "trashy", 3)
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
