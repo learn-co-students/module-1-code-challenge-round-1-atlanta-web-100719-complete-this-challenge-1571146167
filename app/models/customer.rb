fb6c249dc9c8c09126f1ae50c53e3702c620a7ba
clarkj99 <clark@stontech.net>
2019-10-15 10:36:24 -0400

    Code Review 1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,50 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
-    @last_name  = last_name
+    @last_name = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(restaurant, self, content, rating)
+  end
+
+  def reviews
+    Review.all.select { |review| review.customer == self }
+  end
+
+  def num_reviews
+    reviews.length
+  end
+
+  def restaurants
+    reviews.map { |review| review.restaurant }
+  end
+
+  def self.all_names
+    @@all.map { |customer| customer.first_name + " " + customer.last_name }
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.select { |customer| customer.first_name == name }
+  end
+
+  def self.find_by_name(name)
+    split_name = name.split(" ")
+    first_name = split_name[0]
+    last_name = split_name[1]
+    find_all_by_first_name(first_name).find { |customer| customer.last_name == last_name }
+    # binding.pry
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,29 @@
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
 def reload
-  load 'config/environment.rb'
+  load "config/environment.rb"
 end
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+clark = Customer.new("Clark", "Johnson")
+kathy = Customer.new("Kathy", "Brown")
+samantha = Customer.new("Samantha", "Charles")
+sam2 = Customer.new("Samantha", "Bixby")
+mike = Customer.new("Michael", "Turner")
+
+moes = Restaurant.new("Moe's")
+chilis = Restaurant.new("Chili's")
+diner = Restaurant.new("Metro Diner")
+
+clark.add_review(moes, "I like this place a lot", 4)
+clark.add_review(moes, "The potatoes are excellent.  They were cooked perfectly", 5)
+clark.add_review(moes, "Not so good. Janice was my server", 1)
+kathy.add_review(chilis, "I've had better", 3)
+samantha.add_review(chilis, "The tacos are the best", 5)
+sam2.add_review(chilis, "The tacos are the best", 5)
 
 binding.pry
+
 0 #leave this here to ensure binding.pry isn't the last line
